8ceb40065fc857dc9bca969ffd635df31570ef32
ayoub <ayoubmkdm3@gmail.com>
2020-10-14 19:35:44 +0100

    allowing dailling from the InfoActivity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent dailIntent = new Intent(Intent.ACTION_DIAL);
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        dailIntent.setData(phoneUri);
+        startActivity(dailIntent);
+
+    }
 }
